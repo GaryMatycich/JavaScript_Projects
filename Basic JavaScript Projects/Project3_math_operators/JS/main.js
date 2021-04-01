ce737d9f5615612c6dd405c568c4288ca202dfd0
GaryMatycich <garymatycich@gmail.com>
2021-04-01 14:46:35 -0600

    math operators assignment

---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,51 @@
+function addition() {
+    var result = 5+5;
+    document.getElementById("additionMath").innerHTML = result;
+}
+
+function subtraction() {
+    var result = 5-5;
+    document.getElementById("subtractionMath").innerHTML = result;
+}
+
+function multiplication() {
+    var result = 5*5;
+    document.getElementById("multiplicationMath").innerHTML = result;
+}
+
+function division() {
+    var result = 5/5;
+    document.getElementById("divisionMath").innerHTML = result;
+}
+
+function multipleOperations() {
+    var result = (10-3)*4/2+6;
+    document.getElementById("multipleOpsMath").innerHTML = result;
+}
+
+function modulus() {
+    var result = 100%30;
+    document.getElementById("modulusMath").innerHTML = result;
+}
+
+function negation() {
+    var result = 10;
+    document.getElementById("negationMath").innerHTML = -result;
+}
+
+function increment() {
+    var x = 2.5;
+    x++;
+    document.getElementById("incrementMath").innerHTML = x;
+}
+
+function decrement() {
+    var y = 4.25;
+    y--;
+    document.getElementById("decrementMath").innerHTML = y;
+}
+
+function randomNum() {
+    z = Math.floor(Math.random() * 100);  // utilizing both the math object and the random function
+    document.getElementById("randomMath").innerHTML = z;
+}
