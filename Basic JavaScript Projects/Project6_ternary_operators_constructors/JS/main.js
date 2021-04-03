fb6c9984b17300edc0e1b05a5a97e4d31b9f6640
GaryMatycich <garymatycich@gmail.com>
2021-04-03 09:53:42 -0600

    added notes

--- Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -4,7 +4,7 @@ function ride_Function() {
     Can_ride = (Height < 52) ? "You are too short":"Your are tall enough";  // the ternary operator is being used to choose between 2 options based on a condition 
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
-// -----------------------------------------
+// --------------------------------------------------------------------------------------------
 
 function Vehicle(Make, Model, Year, Color) {      // This function is a contructor for initializing type "vehicle".
     this.Vehicle_Make = Make;                     // "This" keyword refers to the object that will be created by the contructor.
@@ -21,28 +21,29 @@ function myFunction() {                                     // this function is 
     "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model +   // multiple strings are being concatenated using text and perametors we collected from object "Erik"
     " manufactured in " + Erik.Vehicle_Year;
 }
-//---------------------------------------------
+//---------------------------------------------------------------------------------------------
 
-function Hockey_Player(Position, Handeness, Height, Weight) {
+function Hockey_Player(Position, Handeness, Height, Weight) {        // This function is a contructor for initializing type "Hockey_Player".
     this.Hockey_Player_Position = Position;
     this.Hockey_Player_Handeness = Handeness;
     this.Hockey_Player_Height = Height;
     this.Hockey_Player_Weight = Weight;
 }
-var Dylan_Larkin = new Hockey_Player("Center", "Left", 6.1, 198);
+var Dylan_Larkin = new Hockey_Player("Center", "Left", 6.1, 198);     // var Dylan_Larkin is a object of type Hockey_Player and is being given all the required perameters.
 
 function addPlayer() {
     document.getElementById("New_and_This").innerHTML =
      "Dylan_Larkin plays " + Dylan_Larkin.Hockey_Player_Position;
 }
+//----------------------------------------------------------------------------------------------
 
-function d20PlusOne() {
-    document.getElementById("Nested_Function").innerHTML = Rolld20PlusOne();
-    function Rolld20PlusOne() {
-        var Modifier = 1;
-        var Total = 0;
-        function rollD20() {Total = Modifier + Math.floor(Math.random() * 20) + 1}
+function d20PlusOne() {                                                       // this is my top level function in the Nested Functions assignment
+    document.getElementById("Nested_Function").innerHTML = Rolld20PlusOne();  // here I am setting the outcome of my nested function to be the returned value of "Rolld20PlusOne"
+    function Rolld20PlusOne() {                                               // beginning the Rolld20PlusOne function
+        var Modifier = 1;                                                     // creating a variable called "Modifier" that will hold our Plus One value
+        var Total = 0;                                                        // creating a variable called "Total" that will be returned later
+        function rollD20() {Total = Modifier + Math.floor(Math.random() * 20) + 1}    // calculating Total from a random value plus the modifier (adding 1 to prevent rolling 0)
         rollD20();
-        return Total;
+        return Total;                                                         // returning the total back to the html element "Nested_Function".
     }
 }
